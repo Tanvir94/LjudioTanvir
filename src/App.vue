aaddbf9510d87c675b55001c58a4d264ee632877
Tanvir <tave_94@hotmail.com>
2021-09-30 21:13:11 +0200

    added song info

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <header>
+     <h1>My Music</h1>
+     </header>
+
+     <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist}}
+          </span></h2>
+          <div class="control">
+            <button class="prev">Prev</button>
+            <button class="play" v-if="!isPlaying">Play</button>
+            <button class="pause" v-else>Pause</button>
+            <button class="next">Next</button>
+          </div>
+        {{ songs[0].src }}
+        </section>
+
+     </main>
+
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+    songs: [
+      {
+        title: 'Grateful',
+        artist: 'Neffex',
+        src: require('./assets/neffex-grateful.mp3')
+    
+  },
+        {
+
+          title:  'Invincible',
+          artist: 'Deaf Kev',
+          src:  require('./assets/src_assets_deaf-kev-invincible.mp3')
+          }
+          ],
+          player: new Audio()
+          }
+
+  },
+  methods: {
+    play (song) {
+      if (typeof song.src !="undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.isPlaying = true;
+      }
+    },
+  created () {
+  this.current = this.songs[this.index]
+  this.player.src = this.current.src;
+  //this.player.play();
+
+
   }
-}
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+}
